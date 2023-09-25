dcec1ecacc29bfeb4d69e857216fe5dd5984603e
Omar Salloum <omar.salloum@holidu.com>
2023-09-25 16:31:06 +0200

    Reduce verbose logs in dbt parsing

--- dbt_reshaper/reshaper_logs.py
+++ dbt_reshaper/reshaper_logs.py
@@ -1,5 +1,6 @@
 from dbt.events.types import InfoLevel, ErrorLevel
-from dbt.events.functions import fire_event
+from dbt.events.functions import fire_event, Event
+import dbt
 
 class ReshaperInfoLevel(InfoLevel):
     def code(self):
@@ -24,3 +25,13 @@ def fire_error_event(code, message):
     event.code = lambda: code
     event.message = lambda: message
     fire_event(event)
+
+
+original_fire_event = dbt.events.functions.fire_event
+dbt_event_ignore_codes = ['I031', 'I012', 'I011']
+def fire_event_override(e: Event):
+    global dbt_event_ignore_codes
+    if e.code not in dbt_event_ignore_codes:
+        original_fire_event(e)
+
+dbt.events.functions.fire_event = fire_event_override
